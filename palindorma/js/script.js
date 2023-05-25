060722f5796ab278581ce6fefae48ffa18e92459
quintusdev <quintusdev92@gmail.com>
2023-05-25 13:51:27 +0200

    [FEATURES] ES. COMPLETATO

--- palindorma/js/script.js
+++ palindorma/js/script.js
@@ -1,1 +1,22 @@
+//RICHIESTA INSERIMENTO PAROLA ALL'UTENTE
+let parola = prompt('Inserisci una parola qualsiasi');
 
+//CREO LA PAROLA PALINDROMA
+
+//Funzione split
+let arrayParola = parola.split('');
+console.log(arrayParola);
+
+//Funzione reverse
+let arrayReverse = arrayParola.reverse();
+console.log(arrayReverse);
+
+//Funzione Join
+let reverse = arrayReverse.join('');
+console.log(reverse);
+
+if (reverse === parola) {
+    console.log('La parola inserita è PALINDROMA.');
+} else {
+    console.log('La parola inserita NON è palindroma.');
+}
